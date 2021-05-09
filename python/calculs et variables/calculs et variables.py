389a98faff5076086d40251bd70910c81a42ad49
amirali.11 <amirdek511@gmail.com>
2021-05-09 03:22:18 -0400

    :mosque:

--- python/calculs et variables/calculs et variables.py
+++ python/calculs et variables/calculs et variables.py
@@ -5,5 +5,22 @@
 a = 20
 b = 10
 c = 365
-d = ((b*c)+a)
+d = ((b*c) + a)
 print(d)
+
+#Et maintenant que se passe-t-il si un corbeau decouvre ton tresor et
+# entre chaque semaine dans ta chambre pour voler 3 pieces ?
+#Au bout d'une annee de ce jeu-la , combien te resterait-il de pieces ?
+s = 52
+v = 3
+t = 3670
+u = ( t- (v*s))
+print (u)
+
+
+
+
+
+
+
+
