50bc0aabb69b3db9fcbf5553f5bdb797891328b8
amirali.11 <amirdek511@gmail.com>
2021-05-23 20:24:26 -0400

    :apple:

---
+++ python/chaines, listes, tuples et dictionnaires.py
@@ -0,0 +1,13 @@
+
+# la chaine fautive s'ecrit comme ceci
+
+chaine_fautive = 'il dit , "qu'il est genant d'etre etourdi." '
+
+
+# voila la soulution pour affiche correctement la chaine
+# pour resoudre le probleme nous devons utilise le 3 apostrophe (''')
+# depuis de la chaine de caractere et la fin
+
+chaine_correcte = ''' il dit , "qu'il est genant d'etre etourdi."  '''
+
+print(chaine_correcte)
