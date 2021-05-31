da9a71b06b1268fcdb091f4cb034bbd04b85357d
amirali.11 <amirdek511@gmail.com>
2021-05-30 20:23:43 -0400

    :apple:

--- python/chaine (string)/chaines, listes, tuples et dictionnaires.py
+++ python/chaine (string)/chaines, listes, tuples et dictionnaires.py
@@ -1,7 +1,6 @@
 
 # la chaine fautive s'ecrit comme ceci
 
-chaine_fautive = 'il dit , "qu'il est genant d'etre etourdi." '
 
 
 # voila la soulution pour affiche correctement la chaine
@@ -11,3 +10,27 @@ chaine_fautive = 'il dit , "qu'il est genant d'etre etourdi." '
 chaine_correcte = ''' il dit , "qu'il est genant d'etre etourdi."  '''
 
 print(chaine_correcte)
+
+
+## insere des  valeur dans des chaines
+
+# exos 1
+
+tiiro = 1000 # tiiro c'est a dire numero
+
+eerayo = 'wan ku jacaylahay farxia %s jeer' # c'est a dire je vous aimez  farxia 1000 fois
+
+print(eerayo % tiiro)
+
+# exos 2
+
+monscore = 1000
+message = 'tu as obtenu %s points '
+
+print(message % monscore)
+
+# Multiplier  des chaines
+
+print(10 * 'a')
+
+print(1000* 'snif')
