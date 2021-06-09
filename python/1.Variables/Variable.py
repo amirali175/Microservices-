b7bcd753cb37c074cb9a0e1c800bbfecac10e599
amirali.11 <amirdek511@gmail.com>
2021-06-09 14:12:43 -0400

    :mosque:

---
+++ python/1.Variables/Variable.py
@@ -0,0 +1,39 @@
+1# Questions
+
+#Inserer votre nom  dans une variable X et affiche ?
+#et dit le quel type de variables utilisez - vous ?
+
+X = 'Mon nom est Amir ' # j'utilise un type de chaine caractere (string)
+
+
+# Inserer votre age dans une variable Y et affiche ?
+Y = 'je suis 40 ans '
+
+
+# Insere votre nom du pere dans une variable A ?
+
+A = 'nom de mon pere est Abdullahi'
+print(X,Y,A)
+
+#RESOUDRE UN PROBLEME
+
+1#Combien de pieces aurais-tu dans
+# ton coffre a tresor si tu faisais cela pendant un an ?
+
+pieces = 20
+nouvelle_pieces = 10
+annee = 365
+
+print((nouvelle_pieces *annee)+ pieces)
+
+
+#Et maintenant que se passe-t-il si un corbeau decouvre ton tresor
+# et entre chaque semaine dans ta chambre pour voler 3 pieces ?
+#Au bout d'une annee de ce jeu-la , combien te resterait-il de pieces ?
+
+semaine = 52
+pieces_voler = 3
+annee =3670 # pieces je faisais pendant un an
+resultat = (annee-(semaine*pieces_voler))
+
+print(resultat)
