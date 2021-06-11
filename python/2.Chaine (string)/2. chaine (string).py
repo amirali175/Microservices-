bea837803f29c7c5d53111d489b896e11c6bf4ea
amirali.11 <amirdek511@gmail.com>
2021-06-11 13:36:58 -0400

    :apple:

---
+++ python/2.Chaine (string)/2. chaine (string).py
@@ -0,0 +1,15 @@
+# Creer une chaine fred
+
+fred = 'les gorilles ont de grosses narines , pourquoi ? Parce qu\'ils ont de gros doigts !'
+print(fred)
+
+fred = "Elle est rose et a peluche rose !"
+print(fred)
+
+#inserer des valeur dans des chaines
+
+monscore =1000
+
+message = 'tu as obtenus %s points '
+
+print(message % monscore)
