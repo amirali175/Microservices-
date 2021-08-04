1303da194800b5e2f913f5423469c9694339d9db
amirali.11 <amirdek511@gmail.com>
2021-08-03 20:44:41 -0400

    :horse:

---
+++ python/8. Fonction et Module/8. Fonction et Module.py
@@ -0,0 +1,47 @@
+def foncttest (mon_nom):
+    print("Bonjour %s" % (mon_nom))
+foncttest('Amir')
+
+def epargne (argent_poche , petits_boulots, depense ):
+    return argent_poche+ petits_boulots-depense
+print("les resultat  d'epargne est %s "% epargne(10,10,5))
+
+def test ():
+    premiere_variable =10
+    seconde_variable =10
+    return premiere_variable*seconde_variable
+print("le resulat du test est %s "%test())
+
+
+def construiction_vaisseau (canettes):
+    totale_canttes = 0
+    for semaine in range (1,53):
+        totale_canttes = totale_canttes+ canettes
+        print("Semaine %s = %s "% (semaine, totale_canttes))
+construiction_vaisseau(2)
+
+
+
+
+import sys
+print(sys.stdin.readline())
+
+
+
+def age_blague_idiote ( age):
+    if age >= 10 and age <= 13:
+        print('vous etes adult !')
+    else:
+        print('Pardon vous n\'est pas admissible')
+age_blague_idiote(9)
+
+
+def age_blague_idiote ():
+    print('quel age as-tu ?')
+    age = int(sys.stdin.readline())
+    if age >=10 and age <= 13:
+        print('que donne 13+49')
+    else:
+        print('pardon?')
+age_blague_idiote()
+
