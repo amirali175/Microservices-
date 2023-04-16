081431fae49d7e4d7f8bacf20a3b73c7a0057761
Amir Abdullahi <54910751+amirali175@users.noreply.github.com>
2023-04-16 19:18:52 -0400

    :horse:

---
+++ python/7.Boucle/7.1 boucle While.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 16 18:46:07 2023
+
+@author: amirali11
+"""
+
+""" Boucle While """
+
+# Ecrire un programme qui affiche tant que la boucle est vrai 
+
+nombre = 0 
+
+while nombre < 6:
+    
+    print(nombre)
+    
+    nombre+=1
+    
+
+
+
