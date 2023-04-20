25f98620470d49825605b5341de282b71ece02eb
Amir Abdullahi <54910751+amirali175@users.noreply.github.com>
2023-04-20 18:09:02 -0400

    :turtle:

--- python/7.Boucle/7.1 boucle While.py
+++ python/7.Boucle/7.1 boucle While.py
@@ -6,15 +6,77 @@ Created on Sun Apr 16 18:46:07 2023
 """
 
 """ Boucle While """
-
-# Ecrire un programme qui affiche tant que la boucle est vrai 
+"""
+#Ecrivez un script afficher tant que la condition est strictement inferieur le nombre10
 
 nombre = 0
 
-while nombre < 6:
+while nombre <10:
     
     print(nombre)
-    
+    break
     nombre+=1
     
+    """
 
+
+
+y = 0
+x =0 
+while True:
+    
+    y = int(input("Saisiez un nombre Y:"))
+    
+    x = int(input("Saisiez un nombre X:"))
+    
+    if y>x  :
+    
+        print ("Le nombre Y est superieur a le nombre X ")
+        break
+    
+    else:
+        
+        print("rentre le bon numero!")
+
+
+
+    
+ 
+          
+    
+    
+    
+    
+    
+
+
+    
+'''
+nombre = 0
+
+while True:
+    
+    nombre = int(input("Saisiez un nombre entre 0 a 10:"))
+    
+    if nombre > 0 and nombre < 12:
+        
+        print(f'You entered: {nombre}')
+        
+        break
+    
+    else:
+        
+        print('The integer must be in the range 1-5')
+        
+'''
+
+
+
+        
+        
+    
+    
+    
+    
+    
+    
